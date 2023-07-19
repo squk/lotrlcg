a5acb70f6ab5b85892fdef48828ce74627c1d245
Christian Nieves <cnieves@google.com>
2023-07-19 00:58:37 -0500

    whoops missing fields

--- cmd/beornextract/types/card.go
+++ cmd/beornextract/types/card.go
@@ -9,11 +9,21 @@ type HallOfBeornCard struct {
 	SphereCode    string `json:"sphere_code,omitempty"`
 	SphereName    string `json:"sphere_name,omitempty"`
 	Position      int    `json:"position,omitempty"`
+	Code          string `json:"code,omitempty"`
+	Cost          string `json:"cost,omitempty"`
+	Name          string `json:"name,omitempty"`
+	Traits        string `json:"traits,omitempty"`
+	Text          string `json:"text,omitempty"`
+	Flavor        string `json:"flavor,omitempty"`
+	IsUnique      bool   `json:"is_unique,omitempty"`
 	Threat        int    `json:"threat,omitempty"`
 	Willpower     int    `json:"willpower,omitempty"`
 	Attack        int    `json:"attack,omitempty"`
 	Defense       int    `json:"defense,omitempty"`
 	Health        int    `json:"health,omitempty"`
+	Quantity      int    `json:"quantity,omitempty"`
+	DeckLimit     int    `json:"deck_limit,omitempty"`
+	Illustrator   string `json:"illustrator,omitempty"`
 	Octgnid       string `json:"octgnid,omitempty"`
 	HasErrata     bool   `json:"has_errata,omitempty"`
 	URL           string `json:"url,omitempty"`
